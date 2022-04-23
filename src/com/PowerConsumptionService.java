eb1d42e1593c262278ab248751781013528b6e24
Senal Ransara <senal.ransara38@gmail.com>
2022-04-23 16:39:05 +0530

    Configure into maven and add some jar files

--- src/com/PowerConsumptionService.java
+++ src/com/PowerConsumptionService.java
@@ -5,9 +5,12 @@ import model.PowerConsumption;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.Path;
 
-
+@Path("/Power")
 public class PowerConsumptionService {
 
 		PowerConsumption pObj = new PowerConsumption();
@@ -35,4 +38,3 @@ public class PowerConsumptionService {
 		
 	}
 
-}
