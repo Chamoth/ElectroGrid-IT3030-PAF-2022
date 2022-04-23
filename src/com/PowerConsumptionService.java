017a1829e580c5800e73fa0cd96f59538dccc60f
Senal Ransara <senal.ransara38@gmail.com>
2022-04-24 00:28:56 +0530

    added update and delete opertaions

--- src/com/PowerConsumptionService.java
+++ src/com/PowerConsumptionService.java
@@ -4,11 +4,22 @@ package com;
 import model.PowerConsumption;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.Path;
+
+import org.jsoup.Jsoup;
+import org.jsoup.parser.Parser;
+import org.json.JSONObject;
+
+import com.google.gson.*;
+
+
+import org.jsoup.nodes.Document;
 
 @Path("/Power")
 public class PowerConsumptionService {
@@ -19,7 +30,7 @@ public class PowerConsumptionService {
 		@Path("/")
 		@Produces(MediaType.TEXT_HTML)
 		public String readPowerConsumptions() {
-			return pObj.readPowerConsumptions();
+			return pObj.readPowerConsumption();
 		}
 
 		@POST
@@ -28,11 +39,40 @@ public class PowerConsumptionService {
 		@Produces(MediaType.TEXT_PLAIN)
 		public String insertPowerConsumption(
 				
-				@FormParam("usedUnits") String usedUnits, 
-				@FormParam("unitCost") String unitCost
+				@FormParam("usedUnits") int usedUnits, 
+				@FormParam("unitCost") Float unitCost
 				
 				) {
 			String output = pObj.insertPowerConsumption( usedUnits,unitCost);
+			return output;
+		}
+		
+		@PUT
+		@Path("/")
+		@Consumes(MediaType.APPLICATION_JSON)
+		@Produces(MediaType.TEXT_PLAIN)
+		public String updatePowerConsumption(String PowerConsumptionData) {
+			// Convert the input string to a JSON object
+			JsonObject docObject = new JsonParser().parse(PowerConsumptionData).getAsJsonObject();
+			// Read the values from the JSON object
+			String billId = docObject.get("billId").getAsString();
+			int usedUnits = docObject.get("usedUnits").getAsInt();
+			Float unitCost = docObject.get("unitCost").getAsFloat();
+
+			String output = pObj.updatePowerConsumption(billId, usedUnits, unitCost);
+			return output;
+		}
+
+		@DELETE
+		@Path("/")
+		@Consumes(MediaType.APPLICATION_XML)
+		@Produces(MediaType.TEXT_PLAIN)
+		public String deletePowerConsumption(String PowerConsumptionData) {
+			// Convert the input string to an XML document
+			Document doc = Jsoup.parse(PowerConsumptionData, "", Parser.xmlParser());
+			// Read the value from the element <userID>
+			String PowerConsumptionID = doc.select("PowerConsumptionID").text();
+			String output = nObj.deletePowerConsumption(PowerConsumptionID);
 			return output;
 		}
 		
--- src/model/PowerConsumption.java
+++ src/model/PowerConsumption.java
@@ -3,10 +3,13 @@ package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class PowerConsumption {
 	
 	//Database Connection
+	
 	private Connection connect() {
 		Connection con = null;
 
@@ -22,7 +25,8 @@ public class PowerConsumption {
 	}
 	
 	//Insert data into database - Create Operation
-	public String insertPowerConsumption( String billId, int usedUnits) {
+	
+	public String insertPowerConsumption( int usedUnits, Float unitCost) {
 		String output = "";
 		try {
 			Connection con = connect();
@@ -30,24 +34,121 @@ public class PowerConsumption {
 				return "Error while connecting to the database for inserting.";
 			}
 			// create a prepared statement
-			String query = " insert into notices (`billId`,`usedUnits`,`unitCost`)"
+			String query = " insert into powerCon (`billId`,`usedUnits`,`unitCost`)"
 					+ " values (?, ?, ?)";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
 			// binding values
 			preparedStmt.setInt(1, 0);
 			preparedStmt.setInt(2, usedUnits);
-			preparedStmt.setString(3, unitCost);
+			preparedStmt.setFloat(3, unitCost);
 			
 			// execute the statement
 			preparedStmt.execute();
 			con.close();
 			output = "Inserted successfully";
 		} catch (Exception e) {
-			output = "Error while inserting the notices.";
+			output = "Error while inserting the powerConsumption.";
 			System.err.println(e.getMessage());
 		}
 		return output;
 	}
+	
+	//Read data from database - Read Operation
+	
+			public String readPowerConsumption() {
+				String output = "";
+
+				try {
+					Connection con = connect();
+
+					if (con == null) {
+						return "Error while connecting the database for reading";
+					}
+
+					output = "<table border=\"1\"><tr><th>usedUnits</th><th>unitCost</th></tr>";
+
+					String query = "select * from powerCon";
+					Statement stmt = con.createStatement();
+					ResultSet rs = stmt.executeQuery(query);
+
+					while (rs.next()) {
+						String billId = Integer.toString(rs.getInt("billId"));
+						int usedUnits = rs.getInt("usedUnits");
+						Float unitCost = rs.getFloat("unitCost");
+
+						output += "<tr><td>" + usedUnits + "</td>";
+						output += "<td>" + unitCost + "</td>";
+						
+
+						output += "<td><input name=\"btnUpdate\" type =\"button\" value = \"Update\" class = \"btn btn-secondary\"></td>"
+								+ "<td><form method=\"post\" action=\"Notice.jsp\">"
+								+ "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\"class=\"btn btn-danger\">"
+								+ "<input name=\"unitCost\" type=\"hidden\" value=\"" + billId + "\">" + "</form></td></tr>";
+
+					}
+					con.close();
+					
+					output += "</table>";
+				} catch (Exception e) {
+					output = "Error while reading PowerConsumption";
+					System.err.println(e.getMessage());
+				}
+				return output;
+			}
+
+			
+			//Update the database - Update Operation
+			public String updatePowerConsumption(String billId, int usedUnits, float unitCost)
+					{
+				String output = "";
+				try {
+					Connection con = connect();
+					if (con == null) {
+						return "Error while connecting to the database for updating.";
+					}
+					// create a prepared statement
+					String query = "UPDATE powerCon SET usedUnits=?,unitCost=?  WHERE billId=?";
+
+					PreparedStatement preparedStmt = con.prepareStatement(query);
+					// binding values
+					preparedStmt.setInt(1, usedUnits);
+					preparedStmt.setFloat(2, unitCost);
+				    preparedStmt.setInt(3, Integer.parseInt(billId));
+					// execute the statement
+					preparedStmt.execute();
+					con.close();
+					output = "Updated successfully";
+				} catch (Exception e) {
+					output = "Error while updating the Power Consumption.";
+					System.err.println(e.getMessage());
+				}
+				return output;
+			}
+		
+			//Delete entry from the database - Delete Operation
+
+			public String deleteNotice(String billId) {
+				String output = "";
+				try {
+					Connection con = connect();
+					if (con == null) {
+						return "Error while connecting to the database for deleting.";
+					}
+					// create a prepared statement
+					String query = "delete from powerCon where billId=?";
+					PreparedStatement preparedStmt = con.prepareStatement(query);
+					// binding values
+					preparedStmt.setInt(1, Integer.parseInt(billId));
+					// execute the statement
+					preparedStmt.execute();
+					con.close();
+					output = "Deleted successfully";
+				} catch (Exception e) {
+					output = "Error while deleting a Power consumption.";
+					System.err.println(e.getMessage());
+				}
+				return output;
+			}
 	
 	
 }
