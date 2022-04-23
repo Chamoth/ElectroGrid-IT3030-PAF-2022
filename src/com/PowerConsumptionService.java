d19a02338e9d7eb82a4cec2fd5a1f68dd107d9b0
Senal Ransara <senal.ransara38@gmail.com>
2022-04-23 13:45:48 +0530

    service with get and post created

--- src/com/PowerConsumptionService.java
+++ src/com/PowerConsumptionService.java
@@ -1,5 +1,38 @@
 package com;
 
+
+import model.PowerConsumption;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
+import javax.ws.rs.FormParam;
+
+
 public class PowerConsumptionService {
+
+		PowerConsumption pObj = new PowerConsumption();
+
+		@GET
+		@Path("/")
+		@Produces(MediaType.TEXT_HTML)
+		public String readPowerConsumptions() {
+			return pObj.readPowerConsumptions();
+		}
+
+		@POST
+		@Path("/")
+		@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+		@Produces(MediaType.TEXT_PLAIN)
+		public String insertPowerConsumption(
+				
+				@FormParam("usedUnits") String usedUnits, 
+				@FormParam("unitCost") String unitCost
+				
+				) {
+			String output = pObj.insertPowerConsumption( usedUnits,unitCost);
+			return output;
+		}
+		
+	}
 
 }
